7aaf11363876c6c050ca04926e6f1e9f96dbd5bb
chop4687 <chop4687@naver.com>
2019-05-06 13:35:11 +0900

    auto program by one hour

--- code/auto_hour.R
+++ code/auto_hour.R
@@ -1,8 +1,10 @@
 install.packages("taskscheduleR")
 library(taskscheduleR)
-dir = "C:/Users/korea/Documents/test/delta_version.R"
+dir = "C:/Users/korea/Documents/test/delta_version2.R"
 
 taskscheduler_create(taskname = "myfancyscript", rscript = dir,
-                     schedule = "MINUTE", starttime = "09:30", modifier = 60,startdate = format(Sys.Date(), "%Y/%m/%d"))
-taskscheduler_delete(taskname = "myfancyscript")
+                     schedule = "MINUTE", starttime = "13:40", modifier = 60,startdate = format(Sys.Date(), "%Y/%m/%d"))
+
+
+#taskscheduler_delete(taskname = "myfancyscript")
 
