821a1b050adcdf249bc9cb339217f4622f7ba9a2
chop4687 <chop4687@naver.com>
2019-05-06 14:05:10 +0900

    Update auto_hour.R

--- code/auto_hour.R
+++ code/auto_hour.R
@@ -1,9 +1,9 @@
 install.packages("taskscheduleR")
 library(taskscheduleR)
-dir = "C:/Users/korea/Documents/test/delta_version2.R"
+dir = "C:/Users/korea/Documents/test/final_version.R"
 
 taskscheduler_create(taskname = "myfancyscript", rscript = dir,
-                     schedule = "MINUTE", starttime = "13:40", modifier = 60,startdate = format(Sys.Date(), "%Y/%m/%d"))
+                     schedule = "MINUTE", starttime = "14:02", modifier = 60,startdate = format(Sys.Date(), "%Y/%m/%d"))
 
 
 #taskscheduler_delete(taskname = "myfancyscript")
